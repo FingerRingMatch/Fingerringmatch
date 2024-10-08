c6490362e6b8cb0d6ffd63636cd66913ec98831a
Anurag <sanurag4518@gmail.com>
2024-10-08 18:18:53 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -9,41 +9,33 @@ import React, {useState} from 'react'
 export default function Home() {
 
   const [isLoading, setIsLoading] = useState(true);
-  const [loadingSection, setLoadingSection] = useState('hero');
 
-  const loadSections = async () => {
-      
-    await new Promise(resolve => setTimeout(resolve, 1600));
-    setIsLoading(false);
-  };
-  loadSections();
+const loadSections = async () => {
+  await new Promise(resolve => setTimeout(resolve, 1600));
+  setIsLoading(false);
+};
+loadSections();
 
-  
-
-
-const LoadingAnimation = ({ section }: { section: string }) => {
-  switch (section) {
-    case 'hero':
-      return (
-        <motion.div
-          className="w-20 h-20 text-primaryPink"
-          animate={{ rotate: 360, scale: [1, 1.2, 1] }}
-          transition={{ duration: 2, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Image src='/IMG_6789.jpg' height={80} width={80} alt={''}/>
-        </motion.div>
-      );
-   
-  }
+const LoadingAnimation = () => {
+  return (
+    <motion.div
+      className="w-20 h-20 text-primaryPink"
+      animate={{ rotate: 360, scale: [1, 1.2, 1] }}
+      transition={{ duration: 2, repeat: Infinity, ease: "easeInOut" }}
+    >
+      <Image src='/IMG_6789.jpg' height={80} width={80} alt={''}/>
+    </motion.div>
+  );
 };
 
 if (isLoading) {
   return (
     <div className="flex items-center justify-center h-screen bg-primaryPink">
-      <LoadingAnimation section={loadingSection} />
+      <LoadingAnimation />
     </div>
   );
 }
+
 
   return (
     <div>
