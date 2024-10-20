1c003b744796dce3ed730ce0e4155c98979b7aa8
Anurag <sanurag4518@gmail.com>
2024-10-20 20:14:39 +0530

    fixed prisma error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,15 +2,20 @@
 
 import { PrismaClient } from '@prisma/client';
 
+
+let prisma: PrismaClient;
+
 declare global {
-  var prisma: PrismaClient | undefined; // Declare a global variable for PrismaClient
+  // Extending the globalThis object
+  var prisma: PrismaClient | undefined;
+}
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma; // Assign it to global only in development mode
-}
-
-export { prisma}
-
+export { prisma };
